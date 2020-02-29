2bafc8e66ae0862eaa7e94752cb21d4b77817c14
Vadzim Papko <v.papko@sam-solutions.com>
2020-02-29 13:28:42 +0300

    Added Comment project; Created Comment Service

---
+++ src/Foundation/SitecoreMarketplace.Foundation.Comments/Services/CommentService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SitecoreMarketplace.Foundation.Comments.Models;
+
+namespace SitecoreMarketplace.Foundation.Comments.Services
+{
+    class CommentService : ICommentService
+    {
+        static List<Comment> _comments = new List<Comment>()
+        {
+            new Comment(),
+            new Comment(),
+            new Comment(),
+            new Comment()
+        };
+
+        public IEnumerable<Comment> GetComments()
+        {
+            return _comments;
+        }
+
+        public void NewComment(Comment comment)
+        {
+            _comments.Add(comment);
+        }
+    }
+}
