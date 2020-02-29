9cfa12b3d17e10a812212fa74ffa1353aa23e434
Vadim Gusev <vadim.gusev@definition6.com>
2020-02-29 16:38:14 +0300

    Add a few example renderings

---
+++ src/Project/SitecoreMarketplace.Project.Web/Areas/SitecoreMarketplace/Views/TopNavigationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SitecoreMarketplace.Project.Web.Areas.SitecoreMarketplace.Views
+{
+    public class TopNavigationModel
+    {
+    }
+}
