3de251addd58e71a818eef8719fb66a163c3ae14
Vadzim Papko <v.papko@sam-solutions.com>
2020-02-29 12:15:32 +0300

    Added basic Module structure, service

---
+++ src/Foundation/SitecoreMarketplace.Foundation.ModuleDetails/Models/Module.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SitecoreMarketplace.Foundation.ModuleDetails.Models
+{
+    class Module
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public string URL { get; set; }
+        public IEnumerable<string> ScreenShots { get; set; }
+        public IEnumerable<string> Versions { get; set; }
+
+    }
+}
