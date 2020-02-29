42328e8d691ed7e173a99cd201ef3810f4969307
NikolaiYurouski <M.Yurouski@sam-solutions.com>
2020-02-29 12:57:54 +0300

    add SearchService

---
+++ src/Foundation/SitecoreMarketplace.Foundation.Search/Interfaces/ISearchService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using SitecoreMarketplace.Foundation.Search.Models;
+
+namespace SitecoreMarketplace.Foundation.Search.Interfaces
+{
+    interface ISearchService
+    {
+        List<ModuleSearchResult> GetModules();
+        List<ModuleSearchResult> GetModulesByCategory(string category);
+        List<ModuleSearchResult> GetModulesByTextQuery(string textQuery);
+        List<ModuleSearchResult> GetModulesByVersions(List<string> versionsFromFilter);
+    }
+}
