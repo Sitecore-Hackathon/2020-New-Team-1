163153c0802f43034c52b4c90f4ccead0b8743d3
Vadzim Papko <v.papko@sam-solutions.com>
2020-02-29 14:25:57 +0300

    Added Rating proj; Rating Mocked Service

---
+++ src/Foundation/SitecoreMarketplace.Foundation.Rating/Models/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitecoreMarketplace.Foundation.Rating.Models
+{
+    class Rating
+    {
+        public double Average { get; set; }
+        public int Total { get; set; }
+        public Dictionary<int, int> Chart { get; set; }
+        public string ModuleId { get; set; }
+    }
+}
